55982deb90c61a4afed1beca9c3f6cab95603acc
Preston Matheson <pmphoto123@gmail.com>
2018-11-07 11:27:50 -0800

    console statement for grabing the value for date in the input

--- src/components/countdown_form.js
+++ src/components/countdown_form.js
@@ -15,13 +15,20 @@ class CountdownForm extends Component {
   }
 
   handleSubmit = function(props) {
-    console.log(`value for date is: ${props.date}`);
+    console.log(`value for date is: ${this.state[props.date]}`);
+  }
+
+  handleChange = function(props) {
+    return console.log(`value for input is: ${event.target.value}`);
   }
 
   render(){
+    this.inputData = [ 
+      {prop: 'date', state: this.state.date}
+      ]
   return (
     <form onSubmit={this.handleSubmit} id="event-form">
-      <input type="date" id="date-selector"></input>
+      <input type="date" id="date-selector" onChange={this.handleChange.bind(this)}></input>
       <input type="submit" className="generate-button" value="Generate Countdown"></input>
     </form>
     );
